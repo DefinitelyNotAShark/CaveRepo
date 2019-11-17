9b14be78ee601392871ff809ae13d76c55e511a1
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-11-17 17:28:59 -0600

    Added room detection for impossible scene stuff

---
+++ VRCave/Assets/DetectCaveEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCaveEnter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject FirstRoomObject, OusideObject, caveObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //cue door closing
+            //play door closeing sound
+            OusideObject.SetActive(true);//turn on outside room
+            FirstRoomObject.SetActive(false);//turn off first room
+            caveObject.SetActive(false);//turns off the cave so you just have an arch
+        }
+    }
+}
